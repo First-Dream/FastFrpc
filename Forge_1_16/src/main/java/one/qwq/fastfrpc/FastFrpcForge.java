e4b9d9ec0b3d0929c9e1778e0ae6f23c1e46c868
TT432 <2437315224@qq.com>
2022-06-18 19:08:32 +0800

    ConfigGui

--- Forge_1_16/src/main/java/one/qwq/fastfrpc/FastFrpcForge.java
+++ Forge_1_16/src/main/java/one/qwq/fastfrpc/FastFrpcForge.java
@@ -2,7 +2,11 @@ package one.qwq.fastfrpc;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +16,13 @@ public class FastFrpcForge extends Common{
     public FastFrpcForge() {
         setCommon(this);
         new Initialize();
+
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> FastFrpcForge::registerConfig);
+    }
+
+    static void registerConfig() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY,
+                () -> (client, parent) -> ClothConfigBridge.create(parent));
     }
 
     private static final Logger LOGGER = LogManager.getLogger();
